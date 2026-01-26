fb836fd1fffc9143171360e5ca3a69a9e9b00e7b
Heric da Silva Cruz <hericsilvaho@gmail.com>
2026-01-25 22:35:55 -0300

    RVNS: adicionando novas funções a .h do rvns

--- include/hscopt/rvns.h
+++ include/hscopt/rvns.h
@@ -4,19 +4,36 @@
 #include <stddef.h>
 
 #include "hscopt/decoder.h"
+#include "hscopt/rng.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef struct hscopt_rvns_ctx hscopt_rvns_ctx;
 
-hscopt_rvns_ctx *hscopt_hho_create(double *x0, size_t n, size_t k,
+hscopt_rvns_ctx *hscopt_hho_create(const double *x0, size_t dim, size_t k_max,
+                                   unsigned max_iters, unsigned max_threads,
                                    hscopt_decoder_fn decoder,
-                                   hscopt_decode_ctx *dctx);
+                                   hscopt_decode_ctx *dctx, hscopt_rng *rng);
 
-void hscopt_hho_destroy(hscopt_rvns_ctx *ctx);
+void hscopt_rvns_destroy(hscopt_rvns_ctx *ctx);
 
-int hscopt_hho_iterate(hscopt_rvns_ctx *ctx, unsigned iters);
+int hscopt_rvns_reset(hscopt_rvns_ctx *ctx, const double *x0);
+
+int hscopt_rvns_iterate(hscopt_rvns_ctx *ctx, unsigned iters);
+
+double hscopt_rvns_best_fitness(const hscopt_rvns_ctx *ctx);
+
+const double *hscopt_rvns_best_keys(const hscopt_rvns_ctx *ctx);
+
+unsigned hscopt_rvns_iteration(const hscopt_rvns_ctx *ctx);
+
+size_t hscopt_rvns_dim(const hscopt_rvns_ctx *ctx);
+
+size_t hscopt_rvns_k_max(const hscopt_rvns_ctx *ctx);
+
+unsigned hscopt_rvns_max_threads(const hscopt_rvns_ctx *ctx);
 
 #ifdef __cplusplus
 }
